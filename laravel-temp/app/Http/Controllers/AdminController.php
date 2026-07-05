9e39177b3ecd4857c1c69de3a567ed0cee8860ca
Dareean <dmardin@gmail.com>
2026-07-05 09:52:48 +0800

    cleanup: remove brevo debug endpoint, unused imports

--- laravel-temp/app/Http/Controllers/AdminController.php
+++ laravel-temp/app/Http/Controllers/AdminController.php
@@ -5,13 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\Notification;
 use App\Models\Pendaftaran;
 use App\Models\User;
-use App\Mail\NotificationMail;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
